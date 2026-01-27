bd266d07f6957b7554fa1de752e40ae3fb808c6e
Sagar <kakkarsagar03@gmail.com>
2026-01-27 19:13:31 +0530

    loaders_comlete

--- LangchainDocuments/web_based_loader.py
+++ LangchainDocuments/web_based_loader.py
@@ -1,9 +1,24 @@
 from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.document_loaders import TextLoader
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
 
+load_dotenv()
 
-url = "https://www.flipkart.com/mobile-phones-store"
+url = "https://job-boards.greenhouse.io/nutrafol/jobs/4651720005"
 
 loader = WebBaseLoader(url)
 
 docs = loader.load()
-print(docs)
+
+prompt = PromptTemplate(
+    template="{question} from {text}",
+    input_variables=["question", "text"]
+)
+
+model = ChatOpenAI()
+chain = prompt| model
+
+print(chain.invoke({"question": "What is the price", "text":docs[0].page_content}))
